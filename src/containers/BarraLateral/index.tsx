a80567f09600544c755e40caccd2c884eeb4ca18
Maxwel Silva <maxwel101@yahoo.com>
2024-08-22 22:21:14 -0300

    Marcando uma tarefa como concluido

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,57 +1,70 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 import FiltroCard from '../../components/FiltroCard'
 import * as S from './styles'
 import { RootReducer } from '../../store'
 import { alterarTermo } from '../../store/reducers/filtro'
 import * as enums from '../../utils/enums/Tarefa'
-import { Campo } from '../../styles'
+import { Botao, Campo } from '../../styles'
 
-const BarraLateral = () => {
+type Props = {
+  mostrarFiltros: boolean
+}
+
+const BarraLateral = ({ mostrarFiltros }: Props) => {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const { termo } = useSelector((state: RootReducer) => state.filtro)
 
   return (
     <S.Aside>
       <div>
-        <Campo
-          type="text"
-          placeholder="Buscar:"
-          value={termo}
-          onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
-        />
-        <S.Filtros>
-          <FiltroCard
-            valor={enums.Status.PENDENTE}
-            criterio="status"
-            legenda="pendentes"
-            contador={1}
-          />
-          <FiltroCard
-            valor={enums.Status.CONCLUIDA}
-            criterio="status"
-            legenda="concluídas"
-            contador={2}
-          />
-          <FiltroCard
-            valor={enums.Prioridade.URGENTE}
-            criterio="prioridade"
-            legenda="urgentes"
-            contador={3}
-          />
-          <FiltroCard
-            valor={enums.Prioridade.IMPORTANTE}
-            criterio="prioridade"
-            legenda="importantes"
-            contador={4}
-          />
-          <FiltroCard
-            valor={enums.Prioridade.NORMAL}
-            criterio="prioridade"
-            legenda="normal"
-            contador={5}
-          />
-          <FiltroCard criterio="todas" legenda="todas" contador={10} />
-        </S.Filtros>
+        {mostrarFiltros ? (
+          <>
+            <Campo
+              type="text"
+              placeholder="Buscar:"
+              value={termo}
+              onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
+            />
+            <S.Filtros>
+              <FiltroCard
+                valor={enums.Status.PENDENTE}
+                criterio="status"
+                legenda="pendentes"
+                contador={1}
+              />
+              <FiltroCard
+                valor={enums.Status.CONCLUIDA}
+                criterio="status"
+                legenda="concluídas"
+                contador={2}
+              />
+              <FiltroCard
+                valor={enums.Prioridade.URGENTE}
+                criterio="prioridade"
+                legenda="urgentes"
+                contador={3}
+              />
+              <FiltroCard
+                valor={enums.Prioridade.IMPORTANTE}
+                criterio="prioridade"
+                legenda="importantes"
+                contador={4}
+              />
+              <FiltroCard
+                valor={enums.Prioridade.NORMAL}
+                criterio="prioridade"
+                legenda="normal"
+                contador={5}
+              />
+              <FiltroCard criterio="todas" legenda="todas" contador={10} />
+            </S.Filtros>
+          </>
+        ) : (
+          <Botao onClick={() => navigate('/')}>Voltar a lista de tarefas</Botao>
+        )}
       </div>
     </S.Aside>
   )
--- src/containers/Formulario/index.tsx
+++ src/containers/Formulario/index.tsx
@@ -1,12 +1,10 @@
 import { FormEvent, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-
 import { BotaoSalvar, MainContainer, Titulo } from '../../styles'
 import { Campo } from '../../styles'
 import { Form, Opcoes, Opcao } from './styles'
 import * as enums from '../../utils/enums/Tarefa'
-import Tarefa from '../../models/Tarefa'
 import { cadastrar } from '../../store/reducers/tarefas'
 
 const Formulario = () => {
@@ -19,15 +17,14 @@ const Formulario = () => {
 
   const cadastrarTarefa = (evento: FormEvent) => {
     evento.preventDefault()
-    const tarefaParaAdicionar = new Tarefa(
-      titulo,
-      prioridade,
-      enums.Status.PENDENTE,
-      descricao,
-      9
+    dispatch(
+      cadastrar({
+        titulo,
+        prioridade,
+        descricao,
+        status: enums.Status.PENDENTE
+      })
     )
-
-    dispatch(cadastrar(tarefaParaAdicionar))
     navigate('/')
   }
 
